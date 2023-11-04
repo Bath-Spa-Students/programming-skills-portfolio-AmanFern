716dfe77c9c5677ccc1ef135c80cfd51f7f57fbc
AmanFern <amanming300@gmail.com>
2023-11-05 03:02:56 +0400

    chapter 5 practice

---
+++ Chapter 5- Dictionaries/Exercises/dictionarypractice.py
@@ -0,0 +1,8 @@
+var_dic = {'colour' : 'yellow',
+           'name' : 'mango',
+           'flavour': 'sweet'}
+print (var_dic)
+print (var_dic['colour'])
+print (var_dic['flavour'])
+print (var_dic['name'])
+print (var_dic.items())
